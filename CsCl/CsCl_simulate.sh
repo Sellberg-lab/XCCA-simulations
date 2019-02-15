34a013238ffd60b3fda9df02400116cc9bf3905e
cldah <cldah@kth.se>
2019-02-16 00:06:21 +0100

    new plot, updated run-on-cluster-file ith subfolders

--- CsCl/CsCl_simulate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH -o CsCl_simulate_test_noisefree.out
-#SBATCH -e CsCl_simulate.err
-
-HOST=`hostname`
-echo "Node: $HOST" 
-/davinci/Cellar/Python/miniconda3/envs/py2/bin/python test_CsCl_84-X_v6.py -dn None -f test_noisefree -n 5 -pdb 6M90_ed
